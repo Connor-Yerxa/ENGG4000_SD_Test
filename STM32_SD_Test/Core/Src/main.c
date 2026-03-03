b69f2b057eca0fc5111dbfbbc786222dc54c4d01
Connor-Yerxa <cyerxa@unb.ca>
2026-03-03 13:37:55 -0400

    tried with pullup resistors, not working.
    Wiping projject and trying something else.

--- STM32_SD_Test/Core/Src/main.c
+++ STM32_SD_Test/Core/Src/main.c
@@ -460,7 +460,7 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pin = SD_CS_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
   HAL_GPIO_Init(SD_CS_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pin : B6_Pin */
